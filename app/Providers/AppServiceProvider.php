27163905c1eca5b450e366b4d6bc723235bd9027
BrknBlade <frankoriven@gmail.com>
2026-05-25 20:59:38 +0200

    Adjusted providers

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        config(['session.secure' => true]);
+        config(['session.same_site' => 'none']);
     }
 }
